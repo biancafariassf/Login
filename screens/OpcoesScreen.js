0fd2b785aab91a2d83f123195c254da973b0ebbe
biancafariassf <bianca.silva1152@etec.sp.gov.br>
2023-07-26 18:20:15 -0300

    Login do Administrador

---
+++ screens/OpcoesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OpcoesScreen = () => {
+  return (
+    <View>
+      {/* Conteúdo da tela OpcoesScreen */}
+      <Text>Aqui vai ficar os botões que vão para o crud</Text>
+    </View>
+  );
+};
+
+export default OpcoesScreen;
+
